237384d6790d1ec1852cac1b4162139ef83d9661
mallick-portfolio <tamal1714019@gmail.com>
2024-10-20 20:50:58 +0600

    dd

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>hello world!!</h1>
-	  <p>hey!!!</p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h2 className="text-3xl font-bold">Git version control testing</h2>
+	  <p>New line added  df</p>
     </div>
   )
 }
